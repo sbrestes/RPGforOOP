89a213ff63d0ee63e76bdcc439a175e448954c25
sbrestes <restes@sbschools.net>
2024-09-12 15:50:28 -0400

    Added MP to Character and public getters and setters for all Character attributes.

--- src/Character.java
+++ src/Character.java
@@ -4,23 +4,47 @@ public abstract class Character {
     public String name;
     public int maxHP;
     public int hp;
+    public int maxMP;
+    public int mp;
 
     public Character(String name, int maxHP) {
         this.name = name;
         this.maxHP = maxHP;
         this.hp = maxHP;
+        this.maxMP = 0;
+        this.mp = 0;
+    }
+
+    public Character(String name, int maxHP, int maxMP) {
+        this.name = name;
+        this.maxHP = maxHP;
+        this.hp = maxHP;
+        this.maxMP = maxMP;
+        this.mp = maxMP;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public int getMaxHP() {
         return maxHP;
     }
 
-    public int getHP() {
+    public void setMaxHP(int maxHP) {
+        this.maxHP = maxHP;
+    }
+
+    public int getHp() {
         return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
     }
 
     public abstract int getAttackPower();
