9a2235a71dd434ecf82a4f6e04e2992f4c3b61c9
sweenys <Sean36850@gmail.com>
2024-09-17 15:14:00 -0400

    I forgot to add the heal

---
+++ src/HealMove.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class HealMove extends Move{
+
+    public String getTitle(){
+        return "Heal";
+    }
+
+    @Override
+    public ArrayList<Command> use(Character user, Character reciever) {
+        ArrayList<Command> commands = new ArrayList<Command>();
+        commands.add(new MessageCommand(user.getName() + " Heals " + user.getName() + "!"));
+        commands.add(new AttackCommand(-3*user.getAttackPower(), user));
+        commands.add(new CheckAndDisplayStatsCommand());
+        return commands;
+    }
+}
